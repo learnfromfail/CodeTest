37fbd7b278f8b3e9e73df695a9173fee1bc7abb0
K_H_S <learnfromfail@gmail.com>
2020-08-01 15:39:03 +0800

    Add files via upload

--- supermarket.sql
+++ supermarket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2020-08-01 09:05:38
+-- 產生時間： 2020-08-01 09:31:43
 -- 伺服器版本： 10.4.13-MariaDB
 -- PHP 版本： 7.4.8
 
@@ -20,38 +20,41 @@ SET time_zone = "+00:00";
 --
 -- 資料庫： `supermarket`
 --
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `t_branch`
---
--- 讀取資料表 supermarket.t_branch 的結構時出現錯誤： #1932 - Table 'supermarket.t_branch' doesn't exist in engine
--- 讀取資料表 supermarket.t_branch 的資料時出現錯誤： #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `supermarket`.`t_branch`' at line 1
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `t_categoryrate`
---
--- 讀取資料表 supermarket.t_categoryrate 的結構時出現錯誤： #1932 - Table 'supermarket.t_categoryrate' doesn't exist in engine
--- 讀取資料表 supermarket.t_categoryrate 的資料時出現錯誤： #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `supermarket`.`t_categoryrate`' at line 1
+CREATE DATABASE IF NOT EXISTS `supermarket` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `supermarket`;
 
 -- --------------------------------------------------------
 
 --
 -- 資料表結構 `t_product`
 --
--- 讀取資料表 supermarket.t_product 的結構時出現錯誤： #1932 - Table 'supermarket.t_product' doesn't exist in engine
--- 讀取資料表 supermarket.t_product 的資料時出現錯誤： #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `supermarket`.`t_product`' at line 1
 
--- --------------------------------------------------------
+CREATE TABLE `t_product` (
+  `productID` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `category` varchar(10) DEFAULT NULL,
+  `price` decimal(6,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- 資料表結構 `t_receipt`
+-- 已傾印資料表的索引
 --
--- 讀取資料表 supermarket.t_receipt 的結構時出現錯誤： #1932 - Table 'supermarket.t_receipt' doesn't exist in engine
--- 讀取資料表 supermarket.t_receipt 的資料時出現錯誤： #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `supermarket`.`t_receipt`' at line 1
+
+--
+-- 資料表索引 `t_product`
+--
+ALTER TABLE `t_product`
+  ADD PRIMARY KEY (`productID`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `t_product`
+--
+ALTER TABLE `t_product`
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
